06c3d22feafa9d33b8085ed9937537a8dc194fd0
Avery Fairbanks <avery@averyf.dev>
2025-12-21 10:43:57 -0500

    health check working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
-import { AppService } from './app.service';
 
 @Module({
   imports: [BlogModule],
--- src/grpc-client.options.ts
+++ src/grpc-client.options.ts
@@ -1,17 +1,17 @@
 import { ReflectionService } from '@grpc/reflection';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { HealthImplementation, protoPath as healthCheckProtoPath } from 'grpc-health-check';
 import { join } from 'path';
 import { BLOG_PACKAGE_NAME } from './blog/interfaces/blog';
-import { HealthImplementation } from 'grpc-health-check';
 
 export const grpcClientOptions: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
     package: BLOG_PACKAGE_NAME,
-    protoPath: join(__dirname, './blog/blog.proto'),
+    protoPath: [healthCheckProtoPath, join(__dirname, './blog/blog.proto')],
     onLoadPackageDefinition: (pkg, server) => {
-      const relf = new ReflectionService(pkg);
-      relf.addToServer(server);
+      const reflection = new ReflectionService(pkg);
+      reflection.addToServer(server);
 
       const healthImpl = new HealthImplementation({
         '': 'UNKNOWN',
