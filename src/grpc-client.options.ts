169ba27f8c5a11a051a2daac5721aa1f6a8a4bfa
Avery Fairbanks <avery@averyf.dev>
2025-12-22 18:46:53 -0500

    update grpc options to correct find proto file, this may need work in the future

--- src/grpc-client.options.ts
+++ src/grpc-client.options.ts
@@ -1,14 +1,14 @@
 import { ReflectionService } from '@grpc/reflection';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { HealthImplementation, protoPath as healthCheckProtoPath } from 'grpc-health-check';
-import { join } from 'path';
 import { BLOG_PACKAGE_NAME } from './blog/interfaces/blog.interface';
+
 
 export const grpcClientOptions: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
     package: BLOG_PACKAGE_NAME,
-    protoPath: [healthCheckProtoPath, join(__dirname, './blog/blog.proto')],
+    protoPath: [healthCheckProtoPath, './src/assets/blog.proto'],
     onLoadPackageDefinition: (pkg, server) => {
       const reflection = new ReflectionService(pkg);
       reflection.addToServer(server);
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { grpcClientOptions } from './grpc-client.options';
+import { Logger, ShutdownSignal } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     grpcClientOptions,
   );
-  
+
+  app.useLogger(['log', 'error']);
   await app.listen();
 }
 bootstrap();
