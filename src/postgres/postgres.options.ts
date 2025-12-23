c6762313a8cf37dca3a75510d480fd67d282fd62
Avery Fairbanks <avery@averyf.dev>
2025-12-23 09:48:20 -0500

    docker compose working, typeorm connecting

--- src/postgres/postgres.options.ts
+++ src/postgres/postgres.options.ts
@@ -3,8 +3,7 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const pgOptions: TypeOrmModuleOptions = {
   type: 'postgres',
   port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'db',
-  synchronize: true,
+  username: 'admin',
+  password: 'admin',
+  database: 'sbs'
 };
